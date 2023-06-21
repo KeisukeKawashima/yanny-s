bc2a39f32180851daa05c966c01b3cd3dabf316b
k-kawashima <k-kawashima@adventure-inc.co.jp>
2023-06-21 19:00:41 +0900

    repository & services

---
+++ src/app/Domain/Repositories/SmokingSpotRepositoryInterface.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Domain\Repositories;
+
+use App\Domain\Models\SmokingSpot\Entity\SmokingSpot;
+use App\Domain\Models\SmokingSpot\ValueObjects\Location;
+use App\Domain\Models\SmokingSpot\ValueObjects\SmokingSpotType;
+
+interface SmokingSpotRepositoryInterface
+{
+    public function findByLocationAndType(Location $location, SmokingSpotType $type): array;
+}
