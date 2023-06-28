cbc699c9cb09027fb0fa9cb51fea0c43cdf98a9c
k-kawashima <codetrain.keisuke.kawashima>
2023-06-29 08:17:29 +0900

    chage

---
+++ src/app/UseCase/SmokingSpotSeachUseCase.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Application\UseCases;
+
+use App\Domain\Models\SmokingSpot\Entity\SmokingSpot;
+use App\Domain\Models\SmokingSpot\ValueObjects\Location;
+use App\Domain\Models\SmokingSpot\ValueObjects\SmokingSpotType;
+use App\Domain\Services\SmokingSpotSearchService;
+
+class SmokingSpotSearchUseCase
+{
+    private $smokingSpotSearchService;
+
+    public function __construct(SmokingSpotSearchService $smokingSpotSearchService)
+    {
+        $this->smokingSpotSearchService = $smokingSpotSearchService;
+    }
+
+    public function execute(int $latitude, int $longitude, string $type): array
+    {
+        $locationValueObject = new Location($latitude,$longitude);
+        $typeValueObject = new SmokingSpotType($type);
+
+        // サービス層を呼び出して喫煙所を検索
+        $smokingSpots = $this->smokingSpotSearchService->searchByLocationAndType($locationValueObject, $typeValueObject);
+
+        // ドメインモデルの配列を返す
+        return $smokingSpots;
+    }
+}
