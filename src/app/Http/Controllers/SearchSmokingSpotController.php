887c2198f88c4c4ed9695bd05cc6db435904e703
keisuke-kawashima <106750867+KeisukeKawashima@users.noreply.github.com>
2023-07-04 14:26:03 +0900

    Feature/yannys 05 (#10)
    * repository & services
    
    * chage
    
    ---------
    
    Co-authored-by: k-kawashima <k-kawashima@adventure-inc.co.jp>
    Co-authored-by: k-kawashima <codetrain.keisuke.kawashima>

--- src/app/Http/Controllers/SearchSmokingSpotController.php
+++ src/app/Http/Controllers/SearchSmokingSpotController.php
@@ -1,13 +1,31 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Application\UseCases\SmokingSpotSearchUseCase;
 use Illuminate\Http\Request;
 
-/**
- * 
- */
-class SearchSmokingSpotController extends Controller
+class SmokingSpotController extends Controller
 {
-    
+    private $smokingSpotSearchUseCase;
+
+    public function __construct(SmokingSpotSearchUseCase $smokingSpotSearchUseCase)
+    {
+        $this->smokingSpotSearchUseCase = $smokingSpotSearchUseCase;
+    }
+
+    public function search(Request $request)
+    {
+        // リクエストパラメータから位置情報と喫煙所の種類を取得
+        $latitude = $request->input('latitude');
+        $longitude = $request->input('longitude');
+        $type = $request->input('type');
+
+        // UseCase層を呼び出して喫煙所を検索
+        $smokingSpots = $this->smokingSpotSearchUseCase->execute($latitude,$longitude,$type);
+
+        // 検索結果をレスポンスとして返す
+        return response()->json($smokingSpots);
+    }
 }
